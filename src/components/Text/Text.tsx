b28f9264f7dd0272fc551b8e77fa2cd524ba0187
TeXmeijin <i8858garden@gmail.com>
2024-02-06 18:35:17 +0900

    fix: 🐛 [docs] jsdoc

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,3 +1,9 @@
 import { FC, PropsWithChildren } from 'react'
 
+/**
+ * こちらのTextコンポーネントだけ使って、RichTextEditorがTree ShakingでBundleに含まれなければ成功
+ * @param children
+ * @constructor
+ */
+
 export const Text: FC<PropsWithChildren> = ({ children }) => <span>{children}</span>
